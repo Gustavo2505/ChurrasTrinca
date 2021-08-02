d8d19ccb6481fbab5faf447f10d2fb2f0b6f8a15
Gustavo Henrique <gustavo.ribeiro@automab.com.br>
2021-08-02 00:33:20 -0300

    Implementação infinity scroll - AllBbqPage

---
+++ ChurrasTrinca/ChurrasTrinca/Helpers/SearchParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChurrasTrinca.Helpers
+{
+  public class SearchParams
+    {
+
+        public int pages { get; set; }
+    }
+}
--- ChurrasTrinca/ChurrasTrinca/Views/Bbq/AllBbqPage.xaml.cs
+++ ChurrasTrinca/ChurrasTrinca/Views/Bbq/AllBbqPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ChurrasTrinca.Models.ResponseService;
+﻿using ChurrasTrinca.Helpers;
+using ChurrasTrinca.Models.ResponseService;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -15,7 +16,7 @@ namespace ChurrasTrinca.Views.Bbq
     public partial class AllBbqPage : ContentPage
     {
         private ObservableCollection<Models.Bbq> MainList { get; set; }
-
+        private SearchParams _searchParams;
         public AllBbqPage()
         {
 
@@ -38,29 +39,33 @@ namespace ChurrasTrinca.Views.Bbq
         private async void carregaotudoAsync()
         {
 
-            ResponseService<List<Models.Bbq>> responseService = await Services.Service.ServiceClientInstance.GetAllBbq();
+            _searchParams = new SearchParams() { pages = 1 };
 
-            if (responseService.isSucess)
-            {
+            ResponseService<List<Models.Bbq>> responseService = await Services.Service.ServiceClientInstance.GetAllBbq(_searchParams.pages = 1);
+
+            //if (responseService.isSucess)
+           // {
                 MainList = new ObservableCollection<Models.Bbq>(responseService.Data);
                 CVListaDeTarefas.ItemsSource = MainList;
-            }
-            else
-            {
-                await DisplayAlert("erro", "Erro", "ok");
-            }
+            CVListaDeTarefas.RemainingItemsThreshold = 0;
+
+        //    }
+        //   else
+            ///   {
+            //       await DisplayAlert("erro", "Erro", "ok");
+            //   }
 
 
 
-         //   MainList = new ObservableCollection<Models.Bbq>();
-          //  var lst = await Services.Service.ServiceClientInstance.GetAllBbq();
+            //   MainList = new ObservableCollection<Models.Bbq>();
+            //  var lst = await Services.Service.ServiceClientInstance.GetAllBbq();
 
-        //    foreach (Models.Bbq its in lst.Data)
-        //    {
-        //        MainList.Add(its);
-        //    };
+            //    foreach (Models.Bbq its in lst.Data)
+            //    {
+            //        MainList.Add(its);
+            //    };
 
-         //   CVListaDeTarefas.ItemsSource = MainList;
+            //   CVListaDeTarefas.ItemsSource = MainList;
 
         }
 
@@ -70,5 +75,26 @@ namespace ChurrasTrinca.Views.Bbq
         {
             Navigation.PushModalAsync(new Bbq.AddBbqPage());
         }
+
+        private async void InfinitySearch(object sender, EventArgs e)
+        {
+            _searchParams.pages++;
+            ResponseService<List<Models.Bbq>> responseService = await Services.Service.ServiceClientInstance.GetAllBbq(_searchParams.pages);
+
+         //   if (responseService.isSucess)
+         //   {
+                var mainlistFromBbqService = responseService.Data;
+                foreach (Models.Bbq item in mainlistFromBbqService)
+                {
+                    MainList.Add(item);
+                }
+                CVListaDeTarefas.ItemsSource = MainList;
+            }
+          //  else
+          //  {
+          //      await DisplayAlert("erro", "Erro", "ok");
+          //  }
+
+        }
     }
-}
+
