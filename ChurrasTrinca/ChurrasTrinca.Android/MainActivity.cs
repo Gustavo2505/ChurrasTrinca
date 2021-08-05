96c2843b526a98208ee8b0001cfe7de387abaa25
Gustavo Henrique <gustavo.ribeiro@automab.com.br>
2021-08-05 15:42:15 -0300

    Adicionando splashScreen Lottie

--- ChurrasTrinca/ChurrasTrinca.Android/MainActivity.cs
+++ ChurrasTrinca/ChurrasTrinca.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace ChurrasTrinca.Droid
 {
-    [Activity(Label = "ChurrasTrinca", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ChurrasTrinca", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -16,6 +16,7 @@ namespace ChurrasTrinca.Droid
 
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
             base.OnCreate(savedInstanceState);
 
             Window.SetStatusBarColor(new Android.Graphics.Color(0, 143, 185, 255));
